a9c30a3f5af2dc6a6e9ee8e68cdde405812b8a28
urk24cs6014 <podamalanireesha@karunya.edu.in>
2025-02-14 11:56:10 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,5 @@
-def greet(name)
-print ("Hello," = name)
-greet("Alice"
-for i in range(5)
-print("Number:"+i)
+def greet(name):
+    print ("Hello,",name)
+greet("Alice")
+for i in range(5):
+    print("Number:",+i)
